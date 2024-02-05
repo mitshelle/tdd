8d2084783a50def6ff586677523c134631a32ff8
Michelle McGowan <mcgowanlmichelle@gmail.com>
2024-02-04 22:53:13 -0800

    Initial Commit

---
+++ src/counter.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+COUNTERS = {}
+
+# We will use the app decorator and create a route called slash counters.
+# specify the variable in route <name>
+# let Flask know that the only methods that is allowed to called
+# on this function is "POST".
+@app.route('/counters/<name>', methods=['POST'])
+def create_counter(name):
+    """Create a counter"""
+    app.logger.info(f"Request to create counter: {name}")
+    global COUNTERS
+    COUNTERS[name] = 0
+    return {name: COUNTERS[name]}, status.HTTP_201_CREATED
--- tests/test_counter.py
+++ tests/test_counter.py
@@ -10,3 +10,19 @@ how to call the web service and assert what it should return.
 - The service must be able to update a counter by name.
 - The service must be able to read the counter
 """
+
+from unittest import TestCase
+
+# we need to import the unit under test - counter
+from src.counter import app 
+
+# we need to import the file that contains the status codes
+from src import status 
+
+class CounterTest(TestCase):
+    """Counter tests"""
+    def test_create_a_counter(self):
+     """It should create a counter"""
+     client = app.test_client()
+     result = client.post('/counters/foo')
+     self.assertEqual(result.status_code, status.HTTP_201_CREATED)
