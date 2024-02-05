3935a30af4dce65cb8a7b5a75a0402163e45835f
Michelle McGowan <mcgowanlmichelle@gmail.com>
2024-02-05 10:35:35 -0800

    Tests
    - added tests for newly created methods

--- src/counter.py
+++ src/counter.py
@@ -1,5 +1,8 @@
 from flask import Flask
 
+# we need to import the file that contains the status codes
+from src import status 
+
 app = Flask(__name__)
 
 COUNTERS = {}
@@ -13,5 +16,31 @@ def create_counter(name):
     """Create a counter"""
     app.logger.info(f"Request to create counter: {name}")
     global COUNTERS
+    if name in COUNTERS:
+            return {"Message":f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
     COUNTERS[name] = 0
     return {name: COUNTERS[name]}, status.HTTP_201_CREATED
+
+@app.route('/counters/<name>', methods=['PUT'])
+def update_counter(name):
+    """Updtae a counter"""
+    app.logger.info(f"Request to update counter: {name}")
+    global COUNTERS
+    if name not in COUNTERS:
+        COUNTERS[name] = 1
+        return {"Message":f"Counter {name} has not been created"}, status.HTTP_204_NO_CONTENT
+    else:
+        COUNTERS[name] += 1
+        
+    return {name: COUNTERS[name]}, status.HTTP_200_OK
+
+@app.route('/counters/<name>', methods=['GET'])
+def read_counter(name):
+    """Read a counter"""
+    app.logger.info(f"Request to read counter: {name}")
+    global COUNTERS
+    # check if not already exist
+    if name not in COUNTERS:
+        return {"Message":f"Counter {name} has not been created"}, status.HTTP_404_NOT_FOUND
+    else:
+        return {name: COUNTERS[name]}, status.HTTP_200_OK
--- tests/test_counter.py
+++ tests/test_counter.py
@@ -18,11 +18,62 @@ from src.counter import app
 
 # we need to import the file that contains the status codes
 from src import status 
+import json
+
 
 class CounterTest(TestCase):
+
+    def setUp(self):
+        self.client = app.test_client()
+        
     """Counter tests"""
     def test_create_a_counter(self):
      """It should create a counter"""
-     client = app.test_client()
-     result = client.post('/counters/foo')
+     
+     result = self.client.post('/counters/foo')
      self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+
+    def test_duplicate_a_counter(self):
+        """It should return an error for duplicates"""
+        result = self.client.post('/counters/bar')
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+        result = self.client.post('/counters/bar')
+        self.assertEqual(result.status_code, status.HTTP_409_CONFLICT)
+
+    def test_update_a_counter(self):
+        """It should return an error for updates"""
+        # create counter
+        client = app.test_client()
+        result = client.post('/counters/updateCounter')
+        # check return success code
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+        # check baseline
+        baseReq = result.get_json()['updateCounter']
+        self.assertEqual(baseReq, 0)
+        # update counter
+        update = client.put('/counters/updateCounter')
+        # check greater than 0 
+        baseReq = update.get_json()['updateCounter']
+        self.assertEqual(baseReq, 1)
+        # check return success code
+        self.assertEqual(update.status_code, status.HTTP_200_OK)
+        # check if not exist yet
+        update = client.put('/counters/updateCounter2')
+        # check return success code
+        self.assertEqual(update.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_read_a_counter(self):
+        """It should return an error for reading"""
+        # create counter
+        client = app.test_client()
+        client.post('/counters/readCounter')
+        # update counter
+        client.put('/counters/readCounter')
+        getResult = client.get('/counters/readCounter')
+        # check return success code
+        self.assertEqual(getResult.status_code, status.HTTP_200_OK)
+        # does not exist check
+        getResult = client.get('/counters/readCounter2')
+        # check return success code
+        self.assertEqual(getResult.status_code, status.HTTP_404_NOT_FOUND)
+
