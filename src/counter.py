cd7caa2660563f8c7fd2a61a194864abc0b6f575
Michelle McGowan <mcgowm1@unlv.nevada.edu>
2024-02-07 14:55:44 -0800

    Linting Problems 1

--- src/counter.py
+++ src/counter.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 # we need to import the file that contains the status codes
-from src import status 
+from src import status
 
 app = Flask(__name__)
 
@@ -11,36 +11,40 @@ COUNTERS = {}
 # specify the variable in route <name>
 # let Flask know that the only methods that is allowed to called
 # on this function is "POST".
+
+
 @app.route('/counters/<name>', methods=['POST'])
 def create_counter(name):
     """Create a counter"""
     app.logger.info(f"Request to create counter: {name}")
     global COUNTERS
-    if name in COUNTERS: 
-            return {"Message":f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
+    if name in COUNTERS:
+        return {"Message": f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
     COUNTERS[name] = 0
     return {name: COUNTERS[name]}, status.HTTP_201_CREATED
 
+
 @app.route('/counters/<name>', methods=['PUT'])
-def update_counter(name): 
+def update_counter(name):
     """Updtae a counter"""
     app.logger.info(f"Request to update counter: {name}")
     global COUNTERS
-    if name not in COUNTERS: 
+    if name not in COUNTERS:
         COUNTERS[name] = 1
-        return {"Message":f"Counter {name} has not been created"}, status.HTTP_204_NO_CONTENT
+        return {"Message": f"Counter {name} has not been created"}, status.HTTP_204_NO_CONTENT
     else:
         COUNTERS[name] += 1
-        
+
     return {name: COUNTERS[name]}, status.HTTP_200_OK
 
+
 @app.route('/counters/<name>', methods=['GET'])
-def read_counter(name): 
+def read_counter(name):
     """Read a counter"""
     app.logger.info(f"Request to read counter: {name}")
     global COUNTERS
     # check if not already exist
     if name not in COUNTERS: 
-        return {"Message":f"Counter {name} has not been created"}, status.HTTP_404_NOT_FOUND
+        return {"Message": f"Counter {name} has not been created"}, status.HTTP_404_NOT_FOUND
     else:
         return {name: COUNTERS[name]}, status.HTTP_200_OK
